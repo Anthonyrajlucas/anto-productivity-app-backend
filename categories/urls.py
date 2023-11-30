f5d309a3ca6f52b7637c6805a2de15b8b69255d2
Anthonyrajlucas <>
2023-11-30 19:31:04 +0000

    category serializers added

--- categories/urls.py
+++ categories/urls.py
@@ -3,4 +3,6 @@ from categories import views
 
 urlpatterns = [
     path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,9 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
+from django.http import Http404
 
 class CategoryList(APIView):
     def get(self, request):
         categories = Category.objects.all()
-        return Response(categories)
+        serializer = CategorySerializer(categories, many= True)
+        return Response(serializer.data)
+
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            category=Category.objects.get(pk=pk)
+            return category
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        category = self.get_object(pk)
+        serializer=CategorySerializer(category)
+        return Response(serializer.data)
 
