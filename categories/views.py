43ba0c10efe734ee4ad6ba511ce16d77032b3bb8
Anthonyrajlucas <>
2023-11-30 18:59:42 +0000

    category view,model and url added

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Category
 
-# Create your views here.
+class CategoryList(APIView):
+    def get(self, request):
+        categories = Category.objects.all()
+        return Response(categories)
+
