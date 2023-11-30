7601cc268d38a27576a136dde10f4a4af0219b3d
Anthonyrajlucas <>
2023-11-30 19:44:56 +0000

    added put in category

--- categories/views.py
+++ categories/views.py
@@ -3,14 +3,17 @@ from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
 from django.http import Http404
+from rest_framework import status
 
 class CategoryList(APIView):
+
     def get(self, request):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many= True)
         return Response(serializer.data)
 
 class CategoryDetail(APIView):
+    serializer_class = CategorySerializer
     def get_object(self, pk):
         try:
             category=Category.objects.get(pk=pk)
@@ -22,4 +25,13 @@ class CategoryDetail(APIView):
         category = self.get_object(pk)
         serializer=CategorySerializer(category)
         return Response(serializer.data)
+
+    def put (self, request, pk):
+        category =self.get_object(pk)
+        serializer=CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
