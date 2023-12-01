6fbf5132d2090eb3e015786967267995e0050230
Anthonyrajlucas <>
2023-12-01 20:26:56 +0000

    tasks app updated

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+from django.http import Http404
+from rest_framework import status
+
+class TaskList(APIView):
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+        
+class TaskDetail(APIView):
+    serializer_class = TaskSerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(Task, pk=pk)
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
