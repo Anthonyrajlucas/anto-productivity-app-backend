96e6e7849ef2784721a700167d561c0521ca21da
Anthonyrajlucas <>
2023-12-01 10:24:33 +0000

    Added serialier,put,get method for the priority resource

--- priorities/urls.py
+++ priorities/urls.py
@@ -3,4 +3,5 @@ from priorities import views
 
 urlpatterns = [
     path('priorities/', views.PriorityList.as_view()),
+    path('priorities/<int:pk>/', views.PriorityDetail.as_view()),
 ]
--- priorities/views.py
+++ priorities/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Priority
+from .serializers import PrioritySerializer
+from django.http import Http404
+from rest_framework import status
 
 class PriorityList(APIView):
 
@@ -8,3 +11,26 @@ class PriorityList(APIView):
         priorities = Priority.objects.all()
         serializer = PrioritySerializer(priorities, many= True)
         return Response(serializer.data)
+
+class PriorityDetail(APIView):
+    serializer_class = PrioritySerializer
+    def get_object(self, pk):
+        try:
+            priority=Priority.objects.get(pk=pk)
+            return priority
+        except Priority.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        priority = self.get_object(pk)
+        serializer=PrioritySerializer(priority)
+        return Response(serializer.data)
+
+    def put (self, request, pk):
+        priority =self.get_object(pk)
+        serializer=PrioritySerializer(priority, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
