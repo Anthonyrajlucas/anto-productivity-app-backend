9b4b0ab383b54879dfb19a6fa60399c34191e21a
Anthonyrajlucas <>
2023-12-01 12:03:41 +0000

    task model added

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .models import State, Category, Priority
 
-# Create your models here.
+class Task(models.Model):
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField()
+    is_overdue = models.BooleanField(default=False)
+    file_attachment = models.FileField(upload_to='images/', null=True, blank=True)
+    assigned = models.ManyToManyField(User, related_name='tasks')
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['due_date']
