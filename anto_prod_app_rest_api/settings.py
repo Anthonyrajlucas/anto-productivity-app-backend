4750471013e160cc4f30a0b41d53a4a18213c49a
Anthonyrajlucas <>
2023-12-03 15:10:35 +0000

    applied filter

--- anto_prod_app_rest_api/settings.py
+++ anto_prod_app_rest_api/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_filters',
     'rest_framework',
     'profiles',
     'categories',
--- tasks/views.py
+++ tasks/views.py
@@ -8,14 +8,14 @@ from .serializers import TaskSerializer
 from django.http import Http404
 from rest_framework import status
 
-class TaskList(APIView):
+class TaskList(generics.ListAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
 
     filter_backends = [
+        DjangoFilterBackend,
         filters.OrderingFilter,
         filters.SearchFilter,
-        DjangoFilterBackend,
     ]
 
     filterset_fields = [
@@ -33,12 +33,6 @@ class TaskList(APIView):
         'created_at',
         'due_date',
     ]
-
-    def get(self, request):
-        tasks = Task.objects.all()
-            
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
 
 class TaskDetail(APIView):
     serializer_class = TaskSerializer
