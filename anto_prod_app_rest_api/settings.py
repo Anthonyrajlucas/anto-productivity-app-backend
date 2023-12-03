bd9729351313bac6253e63e5096becccc457028e
Anthonyrajlucas <>
2023-12-03 21:36:50 +0000

    error fixed

--- anto_prod_app_rest_api/settings.py
+++ anto_prod_app_rest_api/settings.py
@@ -208,4 +208,6 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
