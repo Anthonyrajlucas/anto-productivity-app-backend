cb1f23a4935aea4bf466369831e100cff0365427
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-16 09:03:40 +0000

    changed debugger settings

--- anto_prod_app_rest_api/settings.py
+++ anto_prod_app_rest_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 os.environ.setdefault("SECRET_KEY", "CreateANEWRandomValueHere")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '8000-anthonyrajlucas-anto-pro-ij37pdrk7p.us2.codeanyapp.com',
                   'localhost', 
