d3615112e34593a3f779ef2367d2032e2942a2f8
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-17 17:32:54 +0000

    resolved issues

--- anto_prod_app_rest_api/settings.py
+++ anto_prod_app_rest_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 os.environ.setdefault("SECRET_KEY", "CreateANEWRandomValueHere")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '8000-anthonyrajlucas-anto-pro-ij37pdrk7p.us2.codeanyapp.com',
                   'localhost', 
@@ -126,13 +126,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-
-if 'postgresql' in socket.gethostbyname(socket.gethostname()):
-    DEBUG_TOOLBAR_CONFIG = {
-        'SHOW_TOOLBAR_CALLBACK': lambda request: True,
-    }
 
 if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
