bb3ee65408ad7756d1b992838e679a7a780433bf
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-20 10:49:29 +0000

    all auth updated

--- anto_prod_app_rest_api/settings.py
+++ anto_prod_app_rest_api/settings.py
@@ -120,7 +120,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
