6a4bc07723716b12f55f6b9bda649687215eac8b
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-29 17:35:41 +0000

    Add validation image in readme.md file

--- priorities/views.py
+++ priorities/views.py
@@ -5,32 +5,33 @@ from .serializers import PrioritySerializer
 from django.http import Http404
 from rest_framework import status
 
+
 class PriorityList(APIView):
 
     def get(self, request):
         priorities = Priority.objects.all()
-        serializer = PrioritySerializer(priorities, many= True)
+        serializer = PrioritySerializer(priorities, many=True)
         return Response(serializer.data)
+
 
 class PriorityDetail(APIView):
     serializer_class = PrioritySerializer
+
     def get_object(self, pk):
         try:
-            priority=Priority.objects.get(pk=pk)
-            return priority
+            return Priority.objects.get(pk=pk)
         except Priority.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         priority = self.get_object(pk)
-        serializer=PrioritySerializer(priority)
+        serializer = PrioritySerializer(priority)
         return Response(serializer.data)
 
-    def put (self, request, pk):
-        priority =self.get_object(pk)
-        serializer=PrioritySerializer(priority, data=request.data)
+    def put(self, request, pk):
+        priority = self.get_object(pk)
+        serializer = PrioritySerializer(priority, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
